7b715d3a82df146440a46c47d1020c913012aa44
Moiano319 <vittoria.adams@gmail.com>
2025-08-26 20:48:13 -0700

    Update Shift_Counter_Final.py

--- Shift_Counter_Final.py
+++ Shift_Counter_Final.py
@@ -9,7 +9,7 @@ st.caption("Counts upshifts/downshifts from a MoTeC i2 CSV. "
 st.caption("Please upload a CSV file that only contains the channels 'Gear' and 'Engine Speed'")
 
 uploaded = st.file_uploader("Upload CSV", type=["csv"])
-st.caption("Any shifts before spm reaches this threshold will not be counted. This eliminates any manual shifting that may occur before the engine starts.")
+st.caption("Any shifts before Engine Speed reaches this threshold will not be counted. This eliminates any manual shifting that may occur before the engine starts.")
 rpm_threshold = st.number_input("RPM start threshold", value=2800.0, step=100.0)
 
 RPM_COL = "Engine Speed"
@@ -93,4 +93,5 @@ if uploaded:
 
 else:
     st.info("Upload a CSV to begin.")
+
 
